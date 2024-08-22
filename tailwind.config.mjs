e9f0e6395e057a73d8507e8d31cc3a5cef0c4009
FrankiNarvaez <frannki03@gmail.com>
2024-08-21 19:25:26 -0500

    Added Home component and Navbar

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+		colors: {
+			'main-color': '#864AF9',
+			'white-color': '#F5F7F8',
+			'grey-color': '#9f9f9f',
+			'black-color': '#1E201E'
+		}
+	},
+	plugins: [],
+}
