bc10d98efc063a5f24d2865dcb30f8489198af53
FrankiNarvaez <frannki03@gmail.com>
2024-08-22 22:54:48 -0500

    Resized projects

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,8 +6,9 @@ export default {
 		colors: {
 			'main-color': '#864AF9',
 			'white-color': '#F5F7F8',
-			'grey-color': '#9f9f9f',
-			'black-color': '#1E201E'
+			'gray-color': '#9f9f9f',
+			'black-color': '#1E201E',
+			'gray-project': '#D4D3D3'
 		}
 	},
 	plugins: [],
