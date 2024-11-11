4e6e9cfa386b4d1a96869ad5d6988fd39a840745
FrankiNarvaez <frannki03@gmail.com>
2024-11-11 13:12:07 -0500

    Changed main color, added type of projects

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,22 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				'main-color': '#008FD4',
-				'white-color': '#F8FCFB',
-				'text-white': '#f8f7ff95',
-				'gray-color': '#9f9f9f',
-				'black-color': '#1E201E',
-				'gray-project': '#D4D3D3',
-				'gray-black': '#334155',
-				'orange-semi-black': '#9C2B10',
-				'orange-semi-white': '#fb5607',
-				'border-color': '#BABABA'
-			}
-		},
-	},
-	plugins: [],
-	darkMode: 'class',
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'main-color': '#8B5DFF',
+        'white-color': '#F8FCFB',
+        'text-white': '#f8f7ff95',
+        'gray-color': '#9f9f9f',
+        'black-color': '#1E201E',
+        'gray-project': '#D4D3D3',
+        'gray-black': '#334155',
+        'orange-semi-black': '#9C2B10',
+        'orange-semi-white': '#fb5607',
+        'border-color': '#BABABA'
+      }
+    },
+  },
+  plugins: [],
+  darkMode: 'class',
 }
